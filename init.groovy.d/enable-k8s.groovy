6481a54d20dce2dd12c34d9bd7eb192d37bd4bb7
Ansil H <ansilh@gmail.com>
2019-07-03 07:07:43 +0530

    k8s config

--- init.groovy.d/enable-k8s.groovy
+++ init.groovy.d/enable-k8s.groovy
@@ -1,20 +1,18 @@
-import jenkins.*
 import jenkins.model.*
-import hudson.*
 import hudson.model.*
 import org.csanchez.jenkins.plugins.kubernetes.*
-import org.csanchez.jenkins.plugins.kubernetes.volumes.workspace.EmptyDirWorkspaceVolume
-import org.csanchez.jenkins.plugins.kubernetes.volumes.HostPathVolume
+//import org.csanchez.jenkins.plugins.kubernetes.volumes.workspace.EmptyDirWorkspaceVolume
+//import org.csanchez.jenkins.plugins.kubernetes.volumes.HostPathVolume
  
 //since kubernetes-1.0
 //import org.csanchez.jenkins.plugins.kubernetes.model.KeyValueEnvVar
-import org.csanchez.jenkins.plugins.kubernetes.PodEnvVar
+//import org.csanchez.jenkins.plugins.kubernetes.PodEnvVar
  
 //change after testing
 //ConfigObject conf = new ConfigSlurper().parse(new File(System.getenv("JENKINS_HOME") + '/jenkins_config/kubernetes.txt').text)
  
 def kc
-try {
+//try {
     println("Configuring k8s")
     if (Jenkins.instance.clouds) {
         kc = Jenkins.instance.clouds.get(0)
@@ -23,6 +21,7 @@ try {
         kc = new KubernetesCloud('Kubernetes')
         Jenkins.instance.clouds.add(kc)
         println "cloud added: ${Jenkins.instance.clouds}"
+        
     }
  
     kc.setContainerCapStr('500')
@@ -30,18 +29,12 @@ try {
     kc.setSkipTlsVerify(true)
     kc.setNamespace('default')
     kc.setJenkinsUrl('http://jenkins')
-//    kc.setCredentialsId('') <<-- WE don't need it as we are running inside k8s
     kc.setRetentionTimeout(5)
-    //since kubernetes-1.0
-//    kc.setConnectTimeout()
     kc.setReadTimeout(0)
-    //since kubernetes-1.0
-//    kc.setMaxRequestsPerHostStr(conf.kubernetes.maxRequestsPerHostStr)
- 
 //    kc = null
     println("Configuring k8s completed")
-}
-finally {
+//}
+//finally {
     //if we don't null kc, jenkins will try to serialise k8s objects and that will fail, so we won't see actual error
-    kc = null
-}
+//    kc = null
+//}
