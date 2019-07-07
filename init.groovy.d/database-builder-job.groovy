5c0410a5ac77c871967f7fae8bc09c464d5216fd
Ansil H <ansilh@gmail.com>
2019-07-08 04:56:47 +0530

    Added image build jobs

--- init.groovy.d/database-builder-job.groovy
+++ init.groovy.d/database-builder-job.groovy
@@ -1,6 +1,6 @@
 #!groovy
 
-/* Jenkins Image build job */
+/* MariaDB Image build job */
 import jenkins.*
 import jenkins.model.*
 import hudson.*
@@ -8,13 +8,13 @@ import hudson.model.*
 import jenkins.model.Jenkins
 import hudson.plugins.git.*;
 
-def scm = new GitSCM("https://github.com/ansilh/jenkins-k8s-master.git")
+def scm = new GitSCM("https://github.com/ansilh/demo-database.git")
 scm.branches = [new BranchSpec("*/master")];
 
 def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
 
 def parent = Jenkins.instance
-def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Jenkins-k8s-builder")
+def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Database-k8s-builder")
 job.definition = flowDefinition
 
 parent.reload()
--- init.groovy.d/middleware-builder-job.groovy
+++ init.groovy.d/middleware-builder-job.groovy
@@ -1,6 +1,6 @@
 #!groovy
 
-/* Jenkins Image build job */
+/* PHP7 fpm Image build job */
 import jenkins.*
 import jenkins.model.*
 import hudson.*
@@ -8,13 +8,13 @@ import hudson.model.*
 import jenkins.model.Jenkins
 import hudson.plugins.git.*;
 
-def scm = new GitSCM("https://github.com/ansilh/jenkins-k8s-master.git")
+def scm = new GitSCM("https://github.com/ansilh/demo-middleware.git")
 scm.branches = [new BranchSpec("*/master")];
 
 def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
 
 def parent = Jenkins.instance
-def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Jenkins-k8s-builder")
+def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Middleware-k8s-builder")
 job.definition = flowDefinition
 
 parent.reload()
